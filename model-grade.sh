7fdd5dfc6daf0256610f6fdbdb4c7728274dffa0
Drew Wheeler <wheelercs@protonmail.com>
2023-08-19 07:28:23 -0400

    Updated Grade Script Comments
    Changed some comments on the grading script to provide more information
    on what stuff does. Also reduced the MAX_FILE_SIZE to 16MB from 64MB to
    hopefully speed up performance when grading.

--- model-grade.sh
+++ model-grade.sh
@@ -2,21 +2,20 @@
 
 # === Directories ==================================================================================
 
-MAL_TRAIN=data/mal_train
-BEN_TRAIN=data/ben_train
-MAL_TEST=data/mal_valid
-BEN_TEST=data/ben_valid
+MAL_TRAIN=grade_data/mal_train
+BEN_TRAIN=grade_data/ben_train
+MAL_TEST=grade_data/mal_valid
+BEN_TEST=grade_data/ben_valid
 
 
-# Size of training batches (default 128); lower this number if training fails
+# Low batch size to help with working on lower-end hardware
 BATCH_SIZE=16
 
-# Number of iterations to train model over (default 10)
+# Just train model for one epoch to gauge performance
 EPOCHS=1
 
-# Largest contiguous block of bytes from an executable model will train on; files larger than this
-# value will be truncated (default 16000000)
-MAX_FILE_SIZE=64000000
+# Largest file size that will be trained on
+MAX_FILE_SIZE=16000000
 
 
 python3 ContinueTraining.py --epochs $EPOCHS --batch_size $BATCH_SIZE --max_len $MAX_FILE_SIZE --checkpoint "epoch_9.checkpoint" --model "MalConv" $MAL_TRAIN $BEN_TRAIN $MAL_TEST $BEN_TEST
