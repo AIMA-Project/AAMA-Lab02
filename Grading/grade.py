68591d8b39de7247de3c4b178cc88b591cae951e
Drew Wheeler <wheelercs@protonmail.com>
2023-09-08 07:16:35 -0500

    Get x and y Data For Extrapolation
    Get data from CSV file needed to extrapolate.

--- Grading/grade.py
+++ Grading/grade.py
@@ -2,12 +2,18 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
-def read_csv (f_name: str = "report.csv"):
+def get_test_results (f_name: str = "report.csv"):
+    x_list: list(int) = []      # Holds epoch number
+    y_list: list(float) = []    # Holds epoch verification accuracy
     with open (f_name) as report:
         report_read = csv.reader (report, delimiter = ',')
-        for row in report_read:
-            print (row)
+        for row in enumerate (report_read):
+            # Skip first row that has the labels
+            if (row[0] != 0):
+                x_list.append (int(row[1][0]) + 1)
+                y_list.append (float(row[1][3]))
+    return x_list, y_list
 
 
 if __name__ == "__main__":
-    read_csv ()
+    x, y = get_test_results ()
