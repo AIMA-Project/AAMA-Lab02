607068a29a2cb2d1898031df75503e219665a1cc
wheeler-cs <wheelercs@protonmail.com>
2024-08-24 08:33:41 -0500

    Merge Linux and Windows Container Creation Script
    Merged the script that creates the container on Windows and Linux into a
    script that can be ran on either platform.

--- docker_setup.ps1
+++ docker_setup.ps1
@@ -1,2 +1,8 @@
+#!/bin/bash
+# Needed for Linux script compatibility
+
+# Build the lab 2 container
 docker build -t lab02-image .
+
+# Launch the container and attach to it
 docker run --name lab02 --oom-kill-disable -it lab02-image /bin/bash
--- docker_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build the container for lab 2
-sudo docker build -t lab02 .
-
-# Launch the container and enter a bash shell
-sudo docker run --name lab2 -it lab02
